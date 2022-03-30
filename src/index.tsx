861b23af5fbd0424a6014f9f3aa36df69628c573
Arrastia <arrastia@pm.me>
2022-03-30 20:20:47 +0200

    UPDATE - adapt index.tsx for react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
 
-import './index.css';
+import { App } from 'App';
 
-import { App } from './App';
+const root = document.getElementById('root') as HTMLElement;
 
-render(
+createRoot(root).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById('root')
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </StrictMode>
 );
