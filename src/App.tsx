043088c54643a83691169e9acc042d23822b9946
Arrastia <arrastia@pm.me>
2022-03-30 20:20:54 +0200

    UPDATE - App routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,33 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyles } from 'GlobalStyles';
+
+import { routes } from 'configuration/routes';
+
+import { Layout } from 'ui/_components/Layout';
+
+import { ProductsList } from 'ui/ProductsList';
+
+import { themeState } from 'ui/_tools/Stores/UserStore';
+
+import { themes } from 'ui/_styles/themes';
+
 export function App() {
-  return <h1 style={{ display: 'flex', justifyContent: 'center' }}>Basic react space</h1>;
+  const selectedTheme = useRecoilValue(themeState);
+
+  return (
+    <ThemeProvider theme={{ ...themes[selectedTheme] }}>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />} path={routes.HOME}>
+            <Route element={<ProductsList />} index={true} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
