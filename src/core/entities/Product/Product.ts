2ca7c826f4309a2c092d6d3fbf05ccde44e64cad
Arrastia <arrastia@pm.me>
2022-03-31 20:25:48 +0200

    UPDATE - Product entity interface

--- src/core/entities/Product/Product.ts
+++ src/core/entities/Product/Product.ts
@@ -1,16 +1,18 @@
 export interface Product {
   id: number;
-  image: string;
+  image?: string;
   price: string;
+  quantity?: number;
   title: string;
   year: string;
 }
 
 export class Product {
-  constructor({ id, image, price, title, year }: Product) {
+  constructor({ id, image, price, quantity, title, year }: Product) {
     this.id = id;
     this.image = image;
     this.price = price;
+    this.quantity = quantity;
     this.title = title;
     this.year = year;
   }
