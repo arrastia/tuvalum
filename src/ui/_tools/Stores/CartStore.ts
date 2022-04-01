d93e5833569cfa10637c05d2c2de13b19e342995
Arrastia <arrastia@pm.me>
2022-04-01 23:44:01 +0200

    ADD - set to CartStore selector

--- src/ui/_tools/Stores/CartStore.ts
+++ src/ui/_tools/Stores/CartStore.ts
@@ -17,6 +17,21 @@ export const cartProductState = selectorFamily({
   key: 'cartProductState',
   get:
     (id: number) =>
-    async ({ get }) =>
-      get(productsState).find(p => p.id === id)
+    async ({ get }) => {
+      const product = get(productsState).find(p => p.id === id);
+      return { ...product, quantity: 1 };
+    },
+  set:
+    (id: number) =>
+    ({ get, set }, iProduct: any) => {
+      // const product = get(productsState).find(p => p.id === id);
+
+      // if (!product) {
+      //   return null;
+      // }
+
+      // set(cartProductState(id), prevState => ({ ...prevState, quantity: prevState.quantity + 1 }));
+      set(cartProductState(id), iProduct);
+      // return { ...product, quantity: (product.quantity || 1) + 1 };
+    }
 });
